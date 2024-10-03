5d31a642a34b6f83b4b375f9d6dc10ba5aed3394
Silas Boutwell <silaslee.b@gmail.com>
2024-10-02 18:30:05 -0700

    Finishing touches on the layout and logic

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameOverScreen({Guesses}) {
-  return <View style={styles.screen}>
-    <View style={styles.container}>
-      <Text style={styles.title}>Game Over</Text>
-      <Text style={styles.subtitle}>Your device took {Guesses} guesses to find your number</Text>
+function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  return (
+    <View style={styles.screen}>
+      <Title style={styles.title}>GAME OVER!</Title>
+      <View style={styles.imageContaienr}>
+        <Image source={require("../assets/images/success.png")} style={styles.image} />
+      </View>
+      <Text style={styles.summaryText}>
+        Your device took <Text style={styles.highlight}>{roundsNumber}</Text> tries to guess the number <Text style={styles.highlight}>{userNumber}</Text>.
+      </Text>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
-  </View>
+  );
 }
 
 export default GameOverScreen;
@@ -16,28 +23,31 @@ export default GameOverScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-    marginTop: 200,
-    marginHorizontal: 16,
-  },
-  container: {
-    backgroundColor: Colors.primary800,
-    borderRadius: 12,
     padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  title: {
+  imageContaienr: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }, 
+  summaryText: {
+    fontFamily: 'open-sans',
     fontSize: 24,
-    fontWeight: 'bold',
-    borderWidth: 2,
-    borderColor: Colors.accent500,
-    padding: 24,
-    borderRadius: 6,
-    color: Colors.accent500,
     textAlign: 'center',
+    marginBottom: 24,
   },
-  subtitle: {
-    color: 'white',
-    marginTop: 16,
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   }
 });
